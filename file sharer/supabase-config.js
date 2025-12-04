100380f91260cd66933e172e9f1e8f8a9676d6d2
DISHANT <bheredishant3@gmail.com>
2025-12-04 21:09:48 +0530

    Delete file sharer directory

--- file sharer/supabase-config.js
+++
@@ -1,9 +0,0 @@
-// supabase-config.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const SUPABASE_URL = "https://xvuhldfcjzqpxjnntenk.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2dWhsZGZjanpxcHhqbm50ZW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODMwOTQsImV4cCI6MjA4MDE1OTA5NH0.KAc1dBjuwdmo8u9nB2Qdzu_JQ4IGkwPUGEGEj6rcNZU";
-
-export const BUCKET_NAME = "files"; // your bucket
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
